ae1e0bb8260ed4d7f488afc5cda113cd0eb6c95d
rameezqazi23 <rameez.qazi23@gmail.com>
2023-08-11 18:26:52 +0500

    create main page with image background changes in every reload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     <div className='flex justify-between p-4 z-[100] absolute w-full'>
         <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
         <div>
-            <button className='text-white pr-4'>SignIn</button>
-            <button className='bg-red-600 px-6 py-2 cursor-pointer text-white rounded'>SignUp</button>
+            <button className='pr-4 text-gray-100 font-bold font-sans'>Sign In</button>
+            <button className='bg-red-600 px-3 py-1 cursor-pointer text-gray-100 font-bold rounded font-sans'>Sign Up</button>
         </div>
         
       
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='text-4xl font-bold text-white text-center'>
-        Home
+        
       
     </div>
   )
