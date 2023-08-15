30e7307936f9aa9b781ff397cd7ac6c27aab3764
rameezqazi23 <rameez.qazi23@gmail.com>
2023-08-15 23:26:37 +0500

    creating .env for securing api keys

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,15 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCk-pnnJcDkhMo-yskVImRTpsexzQUdkWo",
-  authDomain: "netflix-85c2f.firebaseapp.com",
-  projectId: "netflix-85c2f",
-  storageBucket: "netflix-85c2f.appspot.com",
-  messagingSenderId: "1098930043151",
-  appId: "1:1098930043151:web:36596bc6cddcaac2caa67a"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APPID
 };
+
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
